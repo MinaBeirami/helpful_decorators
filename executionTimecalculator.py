dbf8980689e346418aab5b78922abcc45a4525e7
MinaBeirami <mina.beirami96@gmail.com>
2019-08-24 01:54:04 -0400

    calculates the runtime of a program

---
+++ executionTimecalculator.py
@@ -0,0 +1,49 @@
+
+
+from datetime import datetime,timedelta,time
+
+"""
+    third-version
+    simple decorator to measure execution time of a function.
+"""
+
+def decorator(*unit):
+
+    def wrapper(function):
+
+        tic=datetime.now().time().microsecond
+        #print(tic)
+        function()
+        toc=datetime.now().time().microsecond
+        #print(toc)
+        time=toc-tic
+        #print(unit)
+
+        if type(unit) == tuple and len(unit):
+            if unit[0] == 'micros':
+               print('this function took ',time,' micro second')
+               return function
+            elif unit[0] == 'milis':
+                time=time/1000
+                print('this function took ',time,' mili second')
+                return function
+            elif unit[0] == 's':
+               time=time/1000000
+               print('this function took ',time,' second')
+               return function
+
+        print ('this function took ',time,' micro second')
+        return function
+
+    return wrapper
+
+
+@decorator()
+@decorator('milis')
+@decorator('micros')
+@decorator('s')
+def function():
+    print ("i am function")
+    return
+
+
