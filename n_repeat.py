d963d1d768925c7a43a48a5d7c9f7bd3a0d8a194
MinaBeirami <mina.beirami96@gmail.com>
2019-08-27 01:40:58 -0400

    coding style fixed-input of decoratoor fixed

--- n_repeat.py
+++ n_repeat.py
@@ -1,24 +1,21 @@
 
-def n_repeat(n=None):
+def n_repeat(n = 1):
 
     def decorator(function):
 
         def wrapper():
-            function()
-
+            counter = n
+            while counter != 0:
+                counter = counter - 1
+                function()
         return wrapper
+    return decorator
 
-    if n:
-        for i in range (0,n):
-            decorator(n)
-        return
-    else:
-        return decorator
-
-#@n_repeat
+@n_repeat
 @n_repeat()
 @n_repeat(4)
 def say_cheese():
     print('cheeeeeeeeseeee!')
 
 say_cheese()
+
